33c9b46dd2056482276da92c1098ff93f71d4a19
wnirolf <wnirolf@hotmail.com>
2013-03-30 22:33:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirgoApp::Application.config.secret_token = '405e74c35fea8c92d4da873e7609513f69315c1a399c6949d8a8e993928d6d3f523d3652b7104056f20b791cb8035988c94fa89d17e4086479165e5bf5648b45'
